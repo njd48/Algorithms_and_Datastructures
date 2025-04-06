b848f03e3401b0d0aba7c0e08145bed193b6541d
Nicholas Dubicki <158865978+njd48@users.noreply.github.com>
2025-04-06 14:27:02 -0400

    First Graphs

---
+++ GraphTheory/GraphL.cpp
@@ -0,0 +1,112 @@
+
+
+#ifndef GRAPHL_H
+#define GRAPHL_H
+
+// #include <matrix.cpp>
+#include <vector>
+#include "mylinklist.cpp"
+
+class Graph {
+
+    private:
+        int V;
+        int E;
+
+        std::vector< List<int> > Adj;
+
+    public:
+        Graph();
+        ~Graph();
+
+        // basic tool methods
+        int addNode();
+        void addEdge( int, int );
+        void removeEdge( int, int );
+        void removeNode( int );
+
+        // examining methods
+        void displayAdj();
+        void displayState();
+};
+
+Graph::Graph(){
+    V = 0;
+    E = 0;
+    Adj.reserve(V);
+};
+Graph::~Graph(){
+    //for ( int i = 0 ; i < V ; i++ ) 
+   // {
+    //    Adj[i].~List();
+    //}
+    // Does the destructor for List and stdvector automatically take care of this?
+};
+
+
+int Graph::addNode(){
+    V++;
+    Adj.resize(V);
+    return V;
+};
+
+void Graph::addEdge( int u, int v ) {
+    if( u==v ){
+        std::cout << "warning: Connecting node to self is disallowed.  nothing happened.\n";
+        return;
+    }
+    if ( u>V || v>V ) {
+        std::cout << "warning: node not in graph. nothing happened.\n";
+        return;
+    }
+
+    Adj[u].push(v);
+    Adj[v].push(u); //<-- remove if directed graph.
+
+    E++;
+}
+
+void Graph::removeEdge( int u, int v ) {
+    Adj[u].remove(v);
+    Adj[v].remove(u); //<-- remove if directed graph
+    E--;
+}
+
+void Graph::removeNode( int u ) {
+    // Procedure: clear all node u adjacencies
+    // Destroy node u's list
+    // potential problem with std::vector holding an empty slot at position u.
+    // Rearrange the Adj vector
+
+    int v;
+
+    while ( !(Adj[u].isEmpty()) ) { // in directed graphs, i would have to search all v's for connections to u
+        v = Adj[u].pop();
+        Adj[v].remove(u);
+        E--;
+    }
+
+    // It's better to do this the hard way and then
+    // tell the user to use remove() sparingly
+    // due to the computational cost.
+    // --- Insert code to rearrange Adj vector here.
+
+}
+
+void Graph::displayAdj() {
+    for ( int i = 0 ; i<V ; i++ ){
+        std::cout<< "node " << i << ":  adj";
+        Adj[i].display();
+    }
+}
+
+void Graph::displayState()
+{
+    std::cout << "Graph State: V = " << V << ", E = " << E << '\n';
+}
+
+
+/*
+*/
+
+#endif 
---
+++ GraphTheory/mylinklist.cpp
@@ -0,0 +1,182 @@
+
+#ifndef MYLL_H
+#define MYLL_H
+
+#include <iostream>
+
+
+
+template <class T>
+class List { 
+
+    protected: 
+        struct node {
+            T info;
+            struct node *next;
+        };
+        typedef struct node *NODEPTR;
+        NODEPTR listptr;  // Points to first node
+
+    public:
+        List();
+        ~List();
+
+        bool isEmpty();
+        void push( T nu );
+        T pop();
+        void remove( T val );
+
+        void insertAfter( T a, T b ) ;
+
+        void display();
+
+};
+
+template <class T>
+List<T>::List() {
+    listptr = nullptr;
+}
+
+template <class T>
+List<T>::~List() {
+
+    NODEPTR p, q;
+
+    if ( isEmpty() ) {
+        return;
+    }
+
+    p = listptr; 
+    while ( p != nullptr ) {
+        q = p->next;
+        delete p;
+        p = q;
+    }
+}
+
+template <class T>
+bool List<T>::isEmpty() {
+    if (listptr == nullptr) { 
+        return true ;
+    } else { 
+        return false ;
+    }
+}
+
+template <class T>
+void List<T>::push( T nu ) { // Appends to front of list
+
+    //std::cout << "here inside \n";
+
+    NODEPTR p; 
+
+    p = new node;
+
+    p->info = nu;
+    p->next = listptr;
+    listptr = p;
+
+    //std::cout << listptr << '\n';
+    //std::cout << p->next << '\n';
+    //std::cout << p->info << '\n';
+
+    //std::cout << "here end inside \n";
+
+}
+
+template <class T>
+T List<T>::pop( ) { // remove node from front of list
+
+    NODEPTR p;
+    T returnval;
+
+    if (isEmpty()){
+        std::cout << "Error, list is empty.\n";
+        return returnval;
+    }
+
+    p         = listptr;
+    returnval = p->info;
+    listptr   = p->next;
+    delete p;    
+    return returnval;
+
+}
+
+template <class T>
+void  List<T>::remove( T val ) { // search list and remove val   
+
+    if (isEmpty()){
+        std::cout << "Error, list is empty.\n";
+        return;
+    }
+    NODEPTR prev = nullptr;
+    NODEPTR p = listptr;
+    T key; 
+
+    while ( p != nullptr ){
+        
+        if ( p->info == val ){
+            //
+            if ( prev == nullptr) { // head node
+                listptr = p->next;
+            } else {                // interior node
+                prev->next = p->next;
+            }
+            delete p;
+            return;
+        } else { 
+            prev = p;
+            p    = p->next;
+        }   
+    }
+    std::cout << "warning: val was not found to be removed.\n";
+}
+
+template <class T>
+void List<T>::insertAfter( T old, T nu ) { // insert nu after old
+
+    NODEPTR p, q;
+
+    p = listptr;
+    while ( p != nullptr ) {
+        if ( p->info == old ) {
+
+            q = new node;
+            q->info = nu;
+            q->next = p->next;
+            p->next = q;
+
+            return;
+
+        } else {
+            p = p->next;
+        }        
+    }
+
+    std::cout << "error: value, " << old << " was not found in list. Item was not inserted.\n";
+
+}
+
+
+template <class T>
+void List<T>::display( ) {
+
+    if (isEmpty()) {
+        std::cout << "list: [ ]\n"; 
+    } else { 
+        NODEPTR p;
+
+        std::cout << "list: [ ";
+        p = listptr;
+        while ( p != nullptr ) {
+            std::cout << (p->info) << ", ";
+            p = p->next;
+        }
+        std::cout <<  " ]\n";
+    }
+    
+}
+
+
+#endif
